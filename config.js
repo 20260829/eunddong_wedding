681d9337b2918e3d31601bd85566761baa3097a3
20260829 <kbreh@naver.com>
2026-04-28 16:30:39 +0900

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,25 @@ const CONFIG = {
     address: "충남 아산시 배방읍 희망로 100 KTX역사내 2층",
     tel: "041-520-9999",
     mapLinks: {
-      kakao: "https://map.kakao.com/link/map/CA웨딩컨벤션,36.794003,127.104582/",
+      kakao: "<!-- * 카카오맵 - 지도퍼가기 -->
+<!-- 1. 지도 노드 -->
+<div id="daumRoughmapContainer1777361414556" class="root_daum_roughmap root_daum_roughmap_landing"></div>
+
+<!--
+	2. 설치 스크립트
+	* 지도 퍼가기 서비스를 2개 이상 넣을 경우, 설치 스크립트는 하나만 삽입합니다.
+-->
+<script charset="UTF-8" class="daum_roughmap_loader_script" src="https://ssl.daumcdn.net/dmaps/map_js_init/roughmapLoader.js"></script>
+
+<!-- 3. 실행 스크립트 -->
+<script charset="UTF-8">
+	new daum.roughmap.Lander({
+		"timestamp" : "1777361414556",
+		"key" : "muhjwkma4ae",
+		"mapWidth" : "640",
+		"mapHeight" : "360"
+	}).render();
+</script>/",
       naver: "https://map.naver.com/v5/entry/place/31363497/"
     }
   },
