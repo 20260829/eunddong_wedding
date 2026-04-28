7762fb9e8d0345703fd2ddf45ac54d612b8b4da4
20260829 <kbreh@naver.com>
2026-04-28 16:28:42 +0900

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ const CONFIG = {
     address: "충남 아산시 배방읍 희망로 100 KTX역사내 2층",
     tel: "041-520-9999",
     mapLinks: {
-      kakao: "https://map.kakao.com/?placeId=20807979/",
+      kakao: "https://map.kakao.com/link/map/CA웨딩컨벤션,36.794003,127.104582/",
       naver: "https://map.naver.com/v5/entry/place/31363497/"
     }
   },
