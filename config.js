2f6293195d9029124638a32b99aab4f6a291a332
20260829 <kbreh@naver.com>
2026-04-28 17:01:47 +0900

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ const CONFIG = {
     address: "충남 아산시 배방읍 희망로 100 KTX역사내 2층",
     tel: "041-520-9999",
     mapLinks: {
-      kakao: "https://kko.to/v6j_E3k8Xo",
+      kakao: "https://map.kakao.com/?itemId=10423184",
       naver: "https://map.naver.com/v5/entry/place/31363497/"
     }
   },
