ec7f70f6f291f0773849b5f2e61532c285d1016e
20260829 <kbreh@naver.com>
2026-06-06 14:17:32 +0900

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,1 +1,19 @@
-guestbook.js
+// ===============================
+// HTML 버튼 연결
+// ===============================
+document.getElementById("guest-submit").addEventListener("click", async () => {
+  const name = document.getElementById("guest-name").value;
+  const message = document.getElementById("guest-message").value;
+
+  if (!name || !message) return;
+
+  await addGuestbook(name, message);
+
+  document.getElementById("guest-name").value = "";
+  document.getElementById("guest-message").value = "";
+
+  renderGuestbook();
+});
+
+// 처음 로딩 시 목록 불러오기
+renderGuestbook();
